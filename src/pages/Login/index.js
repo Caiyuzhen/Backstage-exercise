fce69d649e8aec84a85c514cfb15e7e3ab0464b5
yuzhen <caiyuzhen00@gmail.com>
2022-06-22 16:36:57 +0800

    Update at 0622

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,34 @@
-function Login() {
+import { Card } from 'antd'
+import logo from '@/assets/logo-redux.png'
+import './index.scss'//导入样式文件
+import { Button, Checkbox, Form, Input } from 'antd';
+
+function Login (){
 	return (
-		<div>
-			Login
+		<div className="login">
+			{/* 渲染 antd 的卡片 */}
+			<Card className="login-container">
+				<img className="login-logo" src={logo} alt="" />
+				
+				<Form>
+					<Form.Item>
+						<Input size="large" placeholder="Input your phone number"/>
+					</Form.Item>
+					<Form.Item>
+						<Input.Password size="large" placeholder="Input the characters"/>
+					</Form.Item>
+					<Form.Item>
+						<Checkbox className="login-checkbox-label"/>
+					</Form.Item>
+				</Form>
+			</Card>
 		</div>
 	)
 }
+
+
+
+
+
 
 export default Login
