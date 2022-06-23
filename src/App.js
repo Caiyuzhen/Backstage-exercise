ac9c2715596e6d22fa160937fc3685e91b8f75b1
Caizen <418590624@qq.com>
2022-06-24 01:48:08 +0800

    🚀Update at 0624 morning

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Button } from 'antd'
 import Login from '@/pages/Login'  //别名路径的方式
 // import Login from './pages/Login'  //传统的导入方式
 import { AuthComponents } from '@/components/AuthComponents'
+import './App.css'
 
 
 function App ()
@@ -12,7 +13,6 @@ function App ()
 		//路由配置（包裹）
 		<BrowserRouter>
 			<div className="App">
-				<Button type="primary">Primary Button</Button>
 				{/* 路由出口 */}
 				<Routes>
 					{/* 创建路由 path 跟组件的对应关系, 注意，🔥🔥这里 Layout 不能写死！要包裹 <AuthComponents> 鉴权看是否登录了！！ */}
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,44 @@
-function Layout() {
-	return (
-		<div>
-			layout
-		</div>
+import { Layout , Menu, Popconfirm } from 'antd'
+import { HomeOutlined, DiffOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons'
+import './index.scss'
+
+const { Header , Sider } = Layout //HLayout 包含了 header Sidebar Content Footer 几个子组件
+
+function mainLayout() {
+	return (		
+		<Layout>		
+			<Header className="header">
+				<div className="logo" />
+				<div className="user-info">
+					<span className="user-name">user.name</span>
+					<span className="user-logout">
+					    <Popconfirm title="Confirm to logout？" okText="Exit" cancelText="Cancel">
+						    {/* popover 弹窗 */}
+						    	<LogoutOutlined /> Exit
+					    </Popconfirm>
+			        </span>
+				</div>
+			</Header>
+			<Layout>
+				<Sider width={200} className="site-layout-background">
+					<Menu
+						className="menu-background"
+						mode="inline"
+						theme="dark"
+						defaultSelectedKeys={['1']}
+						style={{ height: '100%', borderRight: 0 }}
+					>
+						<Menu.Item icon={<HomeOutlined />} key="1" className="menuitem">数据概览</Menu.Item>
+						<Menu.Item icon={<DiffOutlined />} key="2" className="menuitem">内容管理</Menu.Item>
+						<Menu.Item icon={<EditOutlined />} key="3" className="menuitem">发布文章</Menu.Item>
+					</Menu>
+				</Sider>
+				{/* 内容区域 */}
+				<Layout className="layout-content" style={{ padding: 20 }}>Context</Layout>
+			</Layout>
+		</Layout>
+
 	)
 }
 
-export default Layout
+export default mainLayout
