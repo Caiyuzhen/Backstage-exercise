0d7e3b951f49333e6f60b39e9b606cd9a04ba505
yuzhen <caiyuzhen00@gmail.com>
2022-06-24 18:57:03 +0800

    🚀Update at 0624 afternoon

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Login from '@/pages/Login'  //别名路径的方式
 // import Login from './pages/Login'  //传统的导入方式
 import { AuthComponents } from '@/components/AuthComponents'
 import './App.css'
+import Publish from './pages/Publish'
+import Article from './pages/Article'
+import Home from './pages/Home'
 
+
+
+//配置路由——————————————————————————————————————————————————————
 
 function App ()
 {
@@ -13,10 +19,16 @@ function App ()
 		//路由配置（包裹）
 		<BrowserRouter>
 			<div className="App">
-				{/* 路由出口 */}
+				{/* 一级路由出口 */}
 				<Routes>
-					{/* 创建路由 path 跟组件的对应关系, 注意，🔥🔥这里 Layout 不能写死！要包裹 <AuthComponents> 鉴权看是否登录了！！ */}
-					<Route path='/'       element={ <AuthComponents><Layout /></AuthComponents> }>  </Route>
+					{/* 🔥一级路由:创建路由 path 跟组件的对应关系, 注意，🔥🔥这里 Layout 不能写死！要包裹 <AuthComponents> 鉴权看是否登录了！！ */}
+					<Route path='/'   element={ <AuthComponents> <Layout /> </AuthComponents> }>  
+						{/* 🔥二级路由:页面内的路由跳转,index 为默认页面，⚡️⚡️二级路由记得配置路由出口！！！ */}
+						<Route index            element={<Home/>}></Route>
+						<Route path='article'   element={<Article/>}></Route>
+						<Route path='publish'   element={<Publish/>}></Route>
+					</Route>
+					{/*  🔥一级路由 */}
 					<Route path='/login'  element={ <Login /> }>  </Route>
 				</Routes>
 			</div>
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,12 +1,17 @@
 import { Layout , Menu, Popconfirm } from 'antd'
 import { HomeOutlined, DiffOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons'
 import './index.scss'
+import { Outlet,Link } from 'react-router-dom'// ⚡️⚡️配置二级路由出口！
+
+
+
 
 const { Header , Sider } = Layout //HLayout 包含了 header Sidebar Content Footer 几个子组件
 
 function mainLayout() {
 	return (		
-		<Layout>		
+		<Layout>
+			{/* 顶部 */}
 			<Header className="header">
 				<div className="logo" />
 				<div className="user-info">
@@ -19,7 +24,10 @@ function mainLayout() {
 			        </span>
 				</div>
 			</Header>
+			
+			{/* 底部 */}
 			<Layout>
+				{/* 侧边栏 */}
 				<Sider width={200} className="site-layout-background">
 					<Menu
 						className="menu-background"
@@ -28,13 +36,18 @@ function mainLayout() {
 						defaultSelectedKeys={['1']}
 						style={{ height: '100%', borderRight: 0 }}
 					>
-						<Menu.Item icon={<HomeOutlined />} key="1" className="menuitem">数据概览</Menu.Item>
-						<Menu.Item icon={<DiffOutlined />} key="2" className="menuitem">内容管理</Menu.Item>
-						<Menu.Item icon={<EditOutlined />} key="3" className="menuitem">发布文章</Menu.Item>
+						{/* 🔥🔥导入 Link 组件进行包裹, 配置 to='/' 属性来实现点击后的跳转！ */}
+						<Menu.Item icon={<HomeOutlined />} key="1" className="menuitem">   <Link to='/'> Home </Link>  </Menu.Item>
+						<Menu.Item icon={<DiffOutlined />} key="2" className="menuitem">   <Link to='/article'> Article</Link>       </Menu.Item>
+						<Menu.Item icon={<EditOutlined />} key="3" className="menuitem">   <Link to='publish'> Pubilsh </Link>      </Menu.Item>
 					</Menu>
 				</Sider>
-				{/* 内容区域 */}
-				<Layout className="layout-content" style={{ padding: 20 }}>Context</Layout>
+				
+				{/* 内容区 */}
+				<Layout className="layout-content" style={{ padding: 20 }}>
+					{/* ⚡️⚡️在内容区域记的配置二级路由出口！！！ */}
+					<Outlet/>
+				</Layout>
 			</Layout>
 		</Layout>
 
