8b892e286628564fccaf67f8ee683268a0cfc0e5
Caizen <418590624@qq.com>
2022-07-05 01:26:28 +0800

    Update

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,56 @@
 //添加对于 webpack 的自定义配置
-
 const path = require('path')
+const { whenProd, getPlugin, pluginByName } = require('@craco/craco')
+
 
 module.exports = {
-	//webpack 配置
+	//用 craco 来进行 webpack 的配置
 	webpack: {
 	//配置别名
 		alias: {
 			//约定：使用 @ 表示 src 文件的所在路径
 			'@': path.resolve(__dirname, 'src')
+		},
+		//配置 CDN, 减小包体积
+		//定义 webpackConfig 将会自动注入的 webpack 配置对象,可以在这个函数中对它进行详细的自定义配置,只要最后return出去就行
+		configure: (webpackConfig) => {
+			let cdn = {
+			  js: [],
+			  css: []
+			}
+
+			// 只有生产环境才配置
+			whenProd(() => {
+			  // key:需要不参与打包的具体的包
+			  // value: cdn 文件中 挂载于全局的变量名称 为了替换之前在开发环境下通过 import 导入的 react / react-dom
+			  webpackConfig.externals = {
+				react: 'React',
+				'react-dom': 'ReactDOM'
+			  }
+
+			  // 配置现成的cdn 资源数组 现在是公共为了测试, 实际开发的时候 用公司自己花钱买的cdn服务器
+			  cdn = {
+				js: [
+				  'https://cdnjs.cloudflare.com/ajax/libs/react/18.1.0/umd/react.production.min.js',
+				  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.1.0/umd/react-dom.production.min.js',
+				],
+				css: []
+			  }
+			})
+	  
+			// 👇都是为了在 public/index.html(🌍小地球那个文件)  入口文件去配置 htmlWebpackPlugin 插件, 在渲染时候会自动注入上边写的 cdn 资源
+			const { isFound, match } = getPlugin(
+			  webpackConfig,
+			  pluginByName('HtmlWebpackPlugin')
+			)
+	  
+			if (isFound) {
+			  // 找到了HtmlWebpackPlugin的插件
+			  match.userOptions.cdn = cdn
+			}
+	  
+			return webpackConfig
 		}
+		
 	}
 }
--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { BrowserRouter, Routes, Route,unstable_HistoryRouter as HistoryRouter } from 'react-router-dom' // XXX as XXX 是重命名
 import { history } from './utils/history'
-import Layout from './pages/Layout'
-import { Button } from 'antd'
-import Login from '@/pages/Login'  //别名路径的方式
-// import Login from './pages/Login'  //传统的导入方式
-import { AuthComponents } from '@/components/AuthComponents'
+import { AuthComponents } from '@/components/AuthComponents' //自动鉴权
 import './App.css'
-import Publish from './pages/Publish'
-import Article from './pages/Article'
-import Home from './pages/Home'
+
+//👇路由懒加载的方式（按需导入组件）
+import { lazy, Suspense } from 'react'//懒加载组件
+const Login = lazy(()=>import('./pages/Login'))
+const Layout = lazy(()=>import('./pages/Layout'))
+const Home = lazy(()=>import('./pages/Home'))
+const Article = lazy(()=>import('./pages/Article'))
+const Publish = lazy(()=>import('./pages/Publish'))
+
+
+
+//👇没有懒加载的方式
+// import Login from '@/pages/Login'  //别名路径的方式
+// import Layout from './pages/Layout'
+// import Home from './pages/Home'
+// import Article from './pages/Article'
+// import Publish from './pages/Publish'
+
+
+
+//——————————
+// import { Button } from 'antd'
+// import Login from './pages/Login'  //传统的导入方式
 
 
 
@@ -20,20 +36,28 @@ function App (){
 		//路由配置（包裹）,<BrowserRouter>为不带历史记录的包裹方式, <HistoryRouter> 为带历史记录的包裹方式
 		// <BrowserRouter>
 		<HistoryRouter history={history}>
-			<div className="App">
-				{/* 一级路由出口 */}
-				<Routes>
-					{/* 🔥一级路由:创建路由 path 跟组件的对应关系, 注意，🔥🔥这里 Layout 不能写死！要包裹 <AuthComponents> 鉴权看是否登录了！！ */}
-					<Route path='/'   element={ <AuthComponents> <Layout /> </AuthComponents> }>  
-						{/* 🔥二级路由:页面内的路由跳转,index 为默认页面，⚡️⚡️二级路由记得配置路由出口！！！ */}
-						<Route index            element={<Home/>}></Route>
-						<Route path='article'   element={<Article/>}></Route>
-						<Route path='publish'   element={<Publish/>}></Route>
-					</Route>
-					{/*  🔥一级路由 */}
-					<Route path='/login'  element={ <Login /> }>  </Route>
-				</Routes>
-			</div>
+			<Suspense
+				fallback={//可以渲染 loading 组件
+					<div style={{textAlign:'center', marginTop:200}}>
+						Loading...
+					</div>
+				}
+			>
+				<div className="App">
+					{/* 一级路由出口 */}
+					<Routes>
+						{/* 🔥一级路由:创建路由 path 跟组件的对应关系, 注意，🔥🔥这里 Layout 不能写死！要包裹 <AuthComponents> 鉴权看是否登录了！！ */}
+						<Route path='/'   element={ <AuthComponents> <Layout /> </AuthComponents> }>  
+							{/* 🔥二级路由:页面内的路由跳转,index 为默认页面，⚡️⚡️二级路由记得配置路由出口！！！ */}
+							<Route index            element={<Home/>}></Route>
+							<Route path='article'   element={<Article/>}></Route>
+							<Route path='publish'   element={<Publish/>}></Route>
+						</Route>
+						{/*  🔥一级路由 */}
+						<Route path='/login'  element={ <Login /> }>  </Route>
+					</Routes>
+				</div>
+			</Suspense>
 		</HistoryRouter>
 		// </BrowserRouter>
 	)
